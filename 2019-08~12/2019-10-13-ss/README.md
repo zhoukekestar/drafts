cd891639bd30694a718242816ad322ccbfc80a01
pipe.zkk <pipe.zkk@alibaba-inc.com>
2019-10-13 17:00:32 +0800

    Update Code

--- 2019-08~12/2019-10-13-ss/README.md
+++ 2019-08~12/2019-10-13-ss/README.md
@@ -1,4 +1,32 @@
 
-CentOS 7.6
+阿里云 CentOS 7.6
 
+## 购买
+
+* 按量
+* 国外
+* 入门共享（最省钱）
+
+![image](https://user-images.githubusercontent.com/7157346/66713160-9a10f700-edd9-11e9-8ae8-a0acfee5ab54.png)
+
+
+* cent_os
+* 7.6 64 位
+* 关闭安全加固
+* 云盘 20G （省钱）
+
+![image](https://user-images.githubusercontent.com/7157346/66713173-b319a800-edd9-11e9-973f-18193bdcb8aa.png)
+
+
+
+## Feature
+* 卸载阿里云盾和安骑士
+* 安装 pip
+* 安装 shadowsocks
+
+## HOWTO
+
+```sh
+ssh root@xx.xx.xx.xx
 curl https://raw.githubusercontent.com/zhoukekestar/drafts/master/2019-08~12/2019-10-13-ss/ss.sh | bash
+```
--- 2019-08~12/2019-10-13-ss/ss.sh
+++ 2019-08~12/2019-10-13-ss/ss.sh
@@ -1,50 +1,62 @@
-echo '#################################'
+echo "\n\n\n---------------------------------"
 echo "        Install shadowsocks      "
-echo '#################################'
+echo "---------------------------------\n\n\n"
 
 
-echo '#################################'
-echo "      start install python      "
-echo '#################################'
-# install python
+echo "\n\n\n---------------------------------"
+echo "        Uninstall Aliyun Software      "
+echo "---------------------------------\n\n\n"
+- uninstall alyun soft
+- @see https://www.laozuo.org/13023.html
+curl http://update.aegis.aliyun.com/download/uninstall.sh | bash
+curl http://update.aegis.aliyun.com/download/quartz_uninstall.sh | bash
+pkill aliyun-service
+rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+rm -rf /usr/local/aegis*
+
+
+echo "\n\n\n---------------------------------"
+echo "      Start install python      "
+echo "---------------------------------\n\n\n"
+- install python
 yum -y install python-setuptools && easy_install pip
 
-
-echo '#################################'
+echo "\n\n\n---------------------------------"
 echo "  start install shadowsocks      "
-echo '#################################'
-# install shadowsocks
+echo "---------------------------------\n\n\n"
+- install shadowsocks
 pip install shadowsocks -i https://files.pythonhosted.org/
 
-# export PATH
-# ls /usr/local/bin
+- export PATH
+- ls /usr/local/bin
 export PATH=/usr/bin:/usr/local/bin:$PATH
 
 
-echo '#################################'
-echo "  Get default config to /etc/shadowsocks.json"
-echo '#################################'
-# get config form git
+echo "\n\n\n---------------------------------"
+echo "  Get default config & set PASSWORD"
+echo "---------------------------------\n\n\n"
+- get config form git
 
-# get current ip address
-# IP=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2)
+- get current ip address
+- IP=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2)
 IP=`curl -s https://api.ipify.org`
 PASSWORD="$IP-$RANDOM"
 
-# set a random password
+- set a random password
 curl https://raw.githubusercontent.com/zhoukekestar/drafts/master/2019-08~12/2019-10-13-ss/passwords.json | sed "s/PASSWORD/$PASSWORD/g" > /etc/shadowsocks.json
 
 
-echo '#################################'
-echo "  start shadowsocks with config  "
-echo '#################################'
-# start shadowsocks
+echo "\n\n\n---------------------------------"
+echo "  Start shadowsocks with config  "
+echo "---------------------------------\n\n\n"
+- start shadowsocks
 ssserver -c /etc/shadowsocks.json -d start
 
 
-echo '#############################'
+echo "\n\n\n---------------------------------"
 echo '       Finished !!!          '
 echo "IP: $IP"
 echo 'PORT: 8383, 8384, 8385'
 echo "PASSWORD: $PASSWORD"
-echo '#############################'
+echo 'MODE: aes-256-cfb'
+echo "---------------------------------\n\n\n"
